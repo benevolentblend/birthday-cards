6a001c6c56611c2b17ba949cd945346e36bd1ad7
Benjamin Thomas <benjamin@thomasnetwork.net>
2021-05-11 23:16:51 -0400

    added Envelope component

--- components/Envelope.tsx
+++ components/Envelope.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import styles from "../styles/Envelope.module.css";
 
 export default function Envelope() {
+  const [isOpen, setOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setOpen(!isOpen);
+  };
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.container} />
+      <div
+        className={`${styles.mail} ${isOpen ? styles.mailOpen : ""}`}
+        role="button"
+        onClick={toggleOpen}
+        onKeyDown={toggleOpen}
+        tabIndex={0}
+      >
+        <div className={styles.cover} />
+        <div className={styles.letter}>
+          @
+        </div>
+      </div>
     </div>
   );
 }
