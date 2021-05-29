90ca0ba0e003868083eb38bbea3d486f7292733c
Benjamin Thomas <benjamin@thomasnetwork.net>
2021-05-28 21:11:56 -0400

    got spring working

--- components/GreetingCardView.tsx
+++ components/GreetingCardView.tsx
@@ -1,22 +1,60 @@
-import { FC } from "react";
+import {
+  FC, useCallback, useEffect, useRef, useState,
+} from "react";
+import { useSpring, animated } from "react-spring";
 import GreetingCard from "../models/GreetingCard";
 import styles from "../styles/Card.module.css";
 
 interface Props {
   greetingCard: GreetingCard;
+  isActive: boolean;
   isOpen: boolean;
 }
 
-const GreetingCardView:FC<Props> = ({ greetingCard, isOpen }) => (
-  <div className={`${styles.wrapper} ${isOpen ? styles.open : ""}`}>
-    <div className={`${styles.frontPage} ${styles.page}`}>
-      <h2>{greetingCard.coverText}</h2>
-    </div>
-    <div className={`${styles.backPage} ${styles.page}`} />
-    <div className={`${styles.insidePage}  ${styles.page}`}>
-      <p>{greetingCard.insideText}</p>
-    </div>
-  </div>
-);
+const windowOffset = 100;
+
+const GreetingCardView:FC<Props> = ({ greetingCard, isActive, isOpen }) => {
+  const wrapperEle = useRef<HTMLDivElement>(null);
+
+  const [delayedIsActive, setDelayedIsActice] = useState(false);
+
+  const getParentPositionBox = useCallback(() => {
+    if (wrapperEle.current?.parentElement) {
+      return wrapperEle.current.parentElement.getBoundingClientRect();
+    }
+    return new DOMRect();
+  }, [wrapperEle]);
+
+  const animatedProps = useSpring({
+    duration: 500,
+    left: delayedIsActive ? -(getParentPositionBox().left - windowOffset) : 0,
+  });
+
+  useEffect(() => {
+    const delay = isActive ? 500 : 0;
+    setTimeout(() => {
+      setDelayedIsActice(isActive);
+    }, delay);
+  }, [isActive]);
+
+  return (
+    <animated.div
+      ref={wrapperEle}
+      className={`${styles.wrapper} ${isActive ? styles.active : ""} ${isOpen ? styles.open : ""}`}
+      style={{
+        left: animatedProps.left,
+      }}
+    >
+      <div className={styles.close} />
+      <div className={`${styles.frontPage} ${styles.page}`}>
+        <h2>{greetingCard.coverText}</h2>
+      </div>
+      <div className={`${styles.backPage} ${styles.page}`} />
+      <div className={`${styles.insidePage}  ${styles.page}`}>
+        <p>{greetingCard.insideText}</p>
+      </div>
+    </animated.div>
+  );
+};
 
 export default GreetingCardView;
--- pages/card.tsx
+++ pages/card.tsx
@@ -3,9 +3,14 @@ import GreetingCardView from "../components/GreetingCardView";
 
 export default function CardPage() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
   const toggleIsOpen = () => {
     setIsOpen(!isOpen);
+  };
+
+  const toggleIsActive = () => {
+    setIsActive(!isActive);
   };
 
   return (
@@ -13,15 +18,14 @@ export default function CardPage() {
       style={{
         marginTop: "5em",
       }}
-      role="button"
-      onClick={toggleIsOpen}
-      onKeyDown={toggleIsOpen}
-      tabIndex={0}
-      aria-label="open"
     >
+      <button type="button" onClick={toggleIsOpen}>Open Card</button>
+      <button type="button" onClick={toggleIsActive}>Make Card Active Card</button>
+
       <GreetingCardView
         greetingCard={{ color: "red", coverText: "Cover Text", insideText: "Inside Text" }}
         isOpen={isOpen}
+        isActive={isActive}
       />
     </div>
   );
